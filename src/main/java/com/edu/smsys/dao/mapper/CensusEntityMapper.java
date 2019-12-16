c3e2d98022aebf15929b6ffb93c73fed4e688908
wangwanghonghongyuyu <874978132@qq.com>
2019-12-16 17:20:41 +0800

    学生信息管理系统 学籍 dao

--- src/main/java/com/edu/smsys/dao/mapper/CensusEntityMapper.java
+++ src/main/java/com/edu/smsys/dao/mapper/CensusEntityMapper.java
@@ -1,7 +1,52 @@
 package com.edu.smsys.dao.mapper;
 
+import com.edu.smsys.dao.entity.CensusEntity;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
 /**
  * 学籍实体映射器
  */
 public interface CensusEntityMapper {
+    /**
+     * 查询所有学籍
+     * @return
+     */
+    public List<CensusEntity> queryCensuses();
+
+    /**
+     * 查询学籍根据学籍id
+     * @param id
+     * @return
+     */
+    public CensusEntity queryClassById(@Param("id") int id);
+
+    /**
+     * 查询学籍根据学籍实体
+     * @param censusEntity
+     * @return
+     */
+    public List<CensusEntity> queryCensusByCensus(CensusEntity censusEntity);
+
+    /**
+     * 修改学籍信息
+     * @param censusEntity
+     * @return
+     */
+    public int updateCensusById(CensusEntity censusEntity);
+
+    /**
+     * 插入学籍信息
+     * @param censusEntity
+     * @return
+     */
+    public int insertCensus(CensusEntity censusEntity);
+
+    /**
+     * 删除班级根据id
+     * @param id
+     * @return
+     */
+    public int deleteCensusById(@Param("id") int id);
 }
--- src/main/java/com/edu/smsys/model/vo/ClassVO.java
+++ src/main/java/com/edu/smsys/model/vo/ClassVO.java
@@ -3,4 +3,5 @@ package com.edu.smsys.model.vo;
  * 给前端页面用的页面对象
  */
 public class ClassVO {
+
 }
